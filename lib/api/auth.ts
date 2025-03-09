5ad473a538aaed9896ead5bb7568c2f33ad2a00c
igara4 <fifty.storm.snowman@gmail.com>
2025-03-09 11:35:40 +0900

    auth.tsにログイン機能作成、ログインページも編集

---
+++ lib/api/auth.ts
@@ -0,0 +1,20 @@
+import { supabase } from "@/lib/supabaseClient"
+import { z } from "zod"
+
+export const LoginSchema= z.object({
+    email:z.string().email("有効なメールアドレスを入力してください"),
+    password:z.string().min(6,"パスワードは6文字以上で入力してください")
+})
+
+export const login =async(email:string,password:string)=>{
+    const{data,error}=await supabase.auth.signInWithPassword({
+        email,
+        password
+    })
+    if(error){
+        console.error("ログインエラー",error.message)
+        return {error:error.message}
+    }
+
+    return {user:data.user}
+}
